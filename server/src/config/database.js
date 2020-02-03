227131dc77469288f377a5d163f3efef2c07cd27
carlosemidio <carlosemidiopereira@gmail.com>
2020-02-03 16:35:54 -0300

    sequelize structure add

---
+++ server/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {  
+    host: 'database',
+    username: 'base_user',
+    password: 'base@2020',
+    database: 'base_db',
+    dialect: 'mariadb',
+    operatorsAliases: false,
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
