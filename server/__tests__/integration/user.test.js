88cbc9dcbbecdf438630a20262f9bccb03edc950
carlosemidio <carlosemidiopereira@gmail.com>
2020-02-16 00:52:23 -0300

    auth middleware for user CRUDL routes

--- server/__tests__/integration/user.test.js
+++ server/__tests__/integration/user.test.js
@@ -1,7 +1,8 @@
 const request = require('supertest');
-
+const factory = require('../factories');
 const app = require('../../src/app');
 const truncate = require('../utils/truncate');
+const { User } = require('../../src/app/models');
 
 describe('Authentication', () => {
     beforeEach(async () => {
@@ -9,45 +10,52 @@ describe('Authentication', () => {
     });
 
     it('should list all users', async () => {
+        const user = await factory.create('User', { 
+            password: 'ielwjlwiuoiwuoiuio1212121' 
+        });
+
         const response = await request(app)
-            .get('/users');
+            .get('/users')
+            .set('Authorization', `Bearer ${user.generateToken()}`);
 
         expect(response.status).toBe(200);
     });
 
     it('should return an user when a valid id is provided', async () => {
+        const user = await factory.create('User', { 
+            password: 'ielwjlwiuoiwuoiuio1212121' 
+        });
+
         const response = await request(app)
-            .post('/users')
-            .send({
-                name: 'test1', 
-                email: 'test1@email.com', 
-                password: 'ielwjlwiuoiwuoiuio1212121'
-            });
+            .get(`/users/${user.id}`)
+            .set('Authorization', `Bearer ${user.generateToken()}`);
 
-        const response2 = await request(app)
-            .get(`/users/${response.body.id}`);
-
-        expect(response2.status).toBe(200);
+        expect(response.status).toBe(200);
     });
 
-    it('should not return an user when a valid id is not provided', async () => {
+    it('should not return any user when a valid id is not provided', async () => {
+        
+        const user = await User.create({ 
+            name: 'test1', 
+            email: 'test1@email.com', 
+            password: 'ielwjlwiuoiwuoiuio1212121' 
+        });
+
         const response = await request(app)
-            .post('/users')
-            .send({
-                name: 'test1', 
-                email: 'test1@email.com', 
-                password: 'ielwjlwiuoiwuoiuio1212121'
-            });
+            .get(`/users/1000000`)
+            .set('Authorization', `Bearer ${user.generateToken()}`);
 
-        const response2 = await request(app)
-            .get(`/users/1000000`);
-
-        expect(response2.status).toBe(401);
+        expect(response.status).toBe(401);
     });
 
     it('should create user with valid credentials', async () => {
+        const user = await factory.create('User', { 
+            password: 'ielwjlwiuoiwuoiuio1212121' 
+        });
+        
         const response = await request(app)
             .post('/users')
+            .set('Authorization', `Bearer ${user.generateToken()}`)
             .send({
                 name: 'test1', 
                 email: 'test1@email.com', 
@@ -58,62 +66,59 @@ describe('Authentication', () => {
     });
 
     it('should not create user with duplicated email', async () => {
+        const user = await User.create({ 
+            name: 'test1', 
+            email: 'test1@email.com', 
+            password: 'ielwjlwiuoiwuoiuio1212121' 
+        });
+        
         const response = await request(app)
             .post('/users')
-            .send({
-                name: 'test1', 
-                email: 'test1@email.com', 
-                password: 'ielwjlwiuoiwuoiuio1212121'
-            });
-        
-        const response2 = await request(app)
-            .post('/users')
+            .set('Authorization', `Bearer ${user.generateToken()}`)
             .send({
                 name: 'test1', 
                 email: 'test1@email.com', 
                 password: 'ielwjlwiuoiwuoiuio1212121'
             });
 
-        expect(response2.status).toBe(401);
+        expect(response.status).toBe(401);
     });
 
     it('should update an user when valid credentials is provided', async () => {
-        const response = await request(app)
-            .post('/users')
-            .send({
-                name: 'test1', 
-                email: 'test1@email.com', 
-                password: 'ielwjlwiuoiwuoiuio1212121'
-            });
+        const user = await User.create({ 
+            name: 'test1', 
+            email: 'test1@email.com', 
+            password: 'ielwjlwiuoiwuoiuio1212121' 
+        });
         
-        const response2 = await request(app)
-            .put(`/users/${response.body.id}`)
+        const response = await request(app)
+            .put(`/users/${user.id}`)
+            .set('Authorization', `Bearer ${user.generateToken()}`)
             .send({
                 name: 'test1.1 (update)', 
                 email: 'test1@email.com', 
                 password: 'ielwjlwiuoiwuoiuio1212121'
             });
 
-        expect(response2.status).toBe(200);
+        expect(response.status).toBe(200);
     });
 
     it('should not update an user when invalid id is provided', async () => {
+        const user = await User.create({ 
+            name: 'test1', 
+            email: 'test1@email.com', 
+            password: 'ielwjlwiuoiwuoiuio1212121' 
+        });
+        
         const response = await request(app)
             .post('/users')
+            .set('Authorization', `Bearer ${user.generateToken()}`)
             .send({
                 name: 'test1', 
                 email: 'test1@email.com', 
                 password: 'ielwjlwiuoiwuoiuio1212121'
             });
-        
-        const response2 = await request(app)
-            .put(`/users/100000`)
-            .send({
-                name: 'test1.1 (update)', 
-                email: 'test1@email.com', 
-                password: 'ielwjlwiuoiwuoiuio1212121'
-            });
 
-        expect(response2.status).toBe(401);
+        expect(response.status).toBe(401);
     });
 });
--- server/src/routes.js
+++ server/src/routes.js
@@ -7,14 +7,14 @@ const authMiddleware = require('./app/middleware/auth')
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
 
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.view);
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
-
-routes.post('/sessions', SessionController.store);
-
-routes.use(authMiddleware);
 
 routes.get('/dashboard', (req, res) => {
     return res.status(200).send();
