6142d183f07546ee272292d7eb49af6a0cedeae3
carlosemidio <carlosemidiopereira@gmail.com>
2020-03-04 05:23:10 -0300

    News CRDL with rich editor

--- client/pages/admin/noticias.js
+++ client/pages/admin/noticias.js
@@ -1,49 +1,132 @@
-import React from "react"
+import React, { Component, Fragment } from 'react'
 import { withAuthSync } from '../../services/auth'
-import {
-  Card,
-  CardContent,
-} from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
 import Layout from '../../components/Layout'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import Link from 'next/link'
+import {
+  withStyles,
+  Typography,
+  Fab,
+  Paper,
+  List,
+  IconButton,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  ListItemSecondaryAction,
+} from '@material-ui/core'
+import { Delete as DeleteIcon, Add as AddIcon } from '@material-ui/icons'
+import { orderBy } from 'lodash'
+import { compose } from 'recompose'
+import moment from 'moment'
 
-const useStyles = makeStyles(theme => ({
-  card: {
-    boxShadow: 'none',
-    paddingLeft: theme.spacing(6),
-    paddingRight: theme.spacing(6),
-    [theme.breakpoints.down('xs')]: {
-      margin: theme.spacing(0),
-      padding: theme.spacing(0),
-    },
-  },
-  cardContent: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  marginTop: {
+import ErrorSnackbar from '../../components/ErrorSnackbar'
+import api from "../../services/api"
+
+const styles = theme => ({
+  news: {
     marginTop: theme.spacing(2),
   },
-}));
+  fab: {
+    position: 'fixed',
+    bottom: theme.spacing(3),
+    right: theme.spacing(3),
+    [theme.breakpoints.down('xs')]: {
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+  },
+})
 
-function News() {
-  const classes = useStyles();
+class News extends Component {
+  state = {
+    loading: true,
+    news: [],
+    error: null,
+  }
 
-  return (
-    <>
-      <CssBaseline />
-      <Layout>
-        <div className='editor'>
-          <Card className={classes.card}>
-            <CardContent className={classes.cardContent}>
-              <h1>Notícias</h1>
-            </CardContent>
-          </Card>
-        </div>
-      </Layout>
-    </>
-  );
+  componentDidMount() {
+    this.getNews()
+  }
+
+  async getNews() {
+    api.get(`/news`)
+    .then(res => {
+      const news = res.data
+      this.setState({ loading: false, news: news || [] })
+    })
+  }
+
+  async deleteNews(news) {
+    if (window.confirm(`Você tem certeza que deseja deletar "${news.title}"`)) {
+      await api.delete(`/news/${news.id}`)
+
+      this.getNews()
+    }
+  }
+
+  render() {
+
+    const { classes } = this.props
+
+    return (
+      <>
+        <CssBaseline />
+        <Layout>
+          <Fragment>
+          {this.state.news.length > 0 ? (
+                <Paper elevation={1} className={classes.news}>
+                  <List>
+                    {orderBy(this.state.news, ['updatedAt', 'title'], ['desc', 'asc']).map(news => (
+                      // <NewsItem news={news} onDelete={this.deleteNews} key={news.id}/>
+                      <>
+                        <Link as={`/admin/noticia/${news.id}`} href={{pathname: '/admin/noticia', query: {id: news.id}}}>
+                          <ListItem key={news.id} button>
+                            <ListItemAvatar>
+                              <Avatar alt={news.title} src={news.image && ('http://localhost:5000/uploads/news/'+news.image)} />
+                            </ListItemAvatar>
+                            <ListItemText
+                              primary={news.title}
+                              secondary={news.updatedAt && `Updated ${moment(news.updatedAt).fromNow()}`}
+                            />
+                            <ListItemSecondaryAction>
+                              <IconButton onClick={() => this.deleteNews(news)} color="inherit">
+                                <DeleteIcon />
+                              </IconButton>
+                            </ListItemSecondaryAction>
+                          </ListItem>
+                        </Link>
+                      </>
+                    ))}
+                  </List>
+                </Paper>
+              ) : (
+                !this.state.loading && <Typography variant="subtitle1">No news to display</Typography>
+              )}
+              <Link as={'/admin/noticia/new'} href={{pathname: '/admin/noticia', query: {id: 'new'}}}>
+                <Fab
+                  color="secondary"
+                  aria-label="add"
+                  className={classes.fab}
+                >
+                  <AddIcon />
+                </Fab>
+              </Link>
+              {this.state.error && (
+                <ErrorSnackbar
+                  onClose={() => this.setState({ error: null })}
+                  message={this.state.error.message}
+                />
+              )}
+          </Fragment>
+        </Layout>
+      </>
+    )
+  }
 }
 
-export default withAuthSync(News);
+export default compose(
+  withAuthSync,
+  withStyles(styles),
+)(News)
